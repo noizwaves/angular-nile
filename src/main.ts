aa09600e551486d26795c7fb6601df57d3ed4a70
Adam Neumann <adam@noizwaves.com>
2016-05-21 18:42:11 -0600

    Fix import error in generated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { NileAppComponent, environment } from './app/';
+import { NileAppComponent, environment } from './app/index';
 
 if (environment.production) {
   enableProdMode();
